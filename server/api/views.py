02ae5cf3aa3a4009b304726064c862a731b2a12f
awshaf <ahmmadawshaf@gmail.com>
2021-08-28 13:55:40 -0700

    run api url request

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+def index(request):
+#    response = User.objects.all()
+   response ='hello dummy text'
+   return HttpResponse(response)
+
 # Create your views here.
